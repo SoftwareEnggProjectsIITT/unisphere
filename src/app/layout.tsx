f44dda62f007fdf4ac351b8f4e37c59cfa5578f6
NillanjanSingh <nillanjan1604@gmail.com>
2025-11-03 21:12:25 +0530

    Add Dark Mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/theme-provider";
+import { cn } from "@/lib/utils";
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -16,10 +18,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <ClerkProvider>
-        <body className={font.className}>
-          {children}
+        <body className={cn(font.className, "bg-white dark:bg-gray-900")}>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
         </body>
       </ClerkProvider>
     </html>
