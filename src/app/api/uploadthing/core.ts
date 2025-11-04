6eeab352b7489b4122ba03272bdf3d08bf85de63
Piyush <piyushkatkar9421@gmail.com>
2025-11-04 15:02:37 +0530

    setup uploadthing

---
+++ src/app/api/uploadthing/core.ts
@@ -0,0 +1,34 @@
+import { currentUser } from "@clerk/nextjs/server";
+import { createUploadthing, type FileRouter } from "uploadthing/next";
+import { UploadThingError } from "uploadthing/server";
+
+const f = createUploadthing();
+console.log("core.ts is loaded");
+
+const handleAuth = async () => {
+  const user = await currentUser();
+
+  console.log("User:", user);
+
+  if (!user) {
+    throw new UploadThingError({ code: "FORBIDDEN" });
+  }
+  console.log("Authenticated user ID:", user.id);
+
+  return { userId: user.id };
+};
+
+export const ourFileRouter = {
+  serverImage: f({ image: { maxFileSize: "4MB", maxFileCount: 1 } })
+    .middleware(() => handleAuth())
+    .onUploadComplete(({ file, metadata }) => {
+      console.log("Upload complete for userId:", metadata.userId);
+      console.log("File URL:", file.ufsUrl);
+    }),
+
+  messageFile: f(["image", "pdf"])
+    .middleware(() => handleAuth())
+    .onUploadComplete(() => {}),
+} satisfies FileRouter;
+
+export type OurFileRouter = typeof ourFileRouter;
