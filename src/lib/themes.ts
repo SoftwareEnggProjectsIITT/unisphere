36156e6a5c46a44fd7cf4feec4d4348396e6bfe1
Sarang Mittal <sm.sarang09@gmail.com>
2025-11-14 13:58:37 +0530

    changed background themes for cards so that font pops out better

--- src/lib/themes.ts
+++ src/lib/themes.ts
@@ -4,44 +4,18 @@ export interface Theme {
 }
 
 export const themes: Theme[] = [
-  {
-    name: "LOVE",
-    gradients: [
-      "linear-gradient(135deg, #ff9a9e 0%, #fad0c4 100%)",
-      "linear-gradient(135deg, #ffdde1 0%, #ee9ca7 100%)",
-      "linear-gradient(135deg, #fbc2eb 0%, #a6c1ee 100%)",
-    ],
-  },
+  { name: "LOVE", gradients: ["linear-gradient(to right, #ff512f, #dd2476)"] },
   {
     name: "REGRET",
-    gradients: [
-      "linear-gradient(135deg, #cfd9df 0%, #e2ebf0 100%)",
-      "linear-gradient(135deg, #e0eafc 0%, #cfdef3 100%)",
-      "linear-gradient(135deg, #d3cce3 0%, #e9e4f0 100%)",
-    ],
+    gradients: ["linear-gradient(to right, #3a7bd5, #3a6073)"],
   },
-  {
-    name: "FUNNY",
-    gradients: [
-      "linear-gradient(135deg, #fbc2eb 0%, #a6c1ee 100%)",
-      "linear-gradient(135deg, #ffecd2 0%, #fcb69f 100%)",
-      "linear-gradient(135deg, #f6d365 0%, #fda085 100%)",
-    ],
-  },
+  { name: "FUNNY", gradients: ["linear-gradient(to right, #f7971e, #ffd200)"] },
   {
     name: "STRESS",
-    gradients: [
-      "linear-gradient(135deg, #f6f9fc 0%, #e9eff5 100%)",
-      "linear-gradient(135deg, #cfd9df 0%, #e2ebf0 100%)",
-      "linear-gradient(135deg, #f5f7fa 0%, #c3cfe2 100%)",
-    ],
+    gradients: ["linear-gradient(to right, #1e3c72, #2a5298)"],
   },
   {
     name: "RANDOM",
-    gradients: [
-      "linear-gradient(135deg, #89f7fe 0%, #66a6ff 100%)",
-      "linear-gradient(135deg, #ff9a9e 0%, #fecfef 100%)",
-      "linear-gradient(135deg, #43e97b 0%, #38f9d7 100%)",
-    ],
+    gradients: ["linear-gradient(to right, #43cea2, #185a9d)"],
   },
 ];
